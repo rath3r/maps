7a046d2fac1a12a28d4fdb83c0cdd02e9e387405
Thomas Meehan <thomas.meehan@living-group.com>
2016-08-03 15:19:13 +0100

    script starter

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,16 @@
-console.log("main");
+// https://addyosmani.com/largescalejavascript/
+
+var App = (function(){
+  var init = function() {
+    console.log("init");
+    test();
+  },
+  test = function (){
+    console.log("another test");
+  };
+  return {
+    init: init
+  }
+}())
+
+App.init();
